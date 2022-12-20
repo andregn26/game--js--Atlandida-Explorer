0e6fd5599854ff09890a3cb27ab366b5fb66d801
andregn26 <andregn26@gmail.com>
2022-12-20 15:04:34 +0000

    Particle rotation done
    Starting to work on some tweaks and fixes

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ window.addEventListener("load", function () {
     update() {
       this.angle += this.velocityOfAngle
       this.speedY += this.gravity
-      this.x -= this.speedX
+      this.x -= this.speedX + this.game.speed
       this.y += this.speedY
       if (this.y > this.game.height + this.size || this.x < 0 - this.size)
         this.markedForDeletion = true
@@ -93,17 +93,21 @@ window.addEventListener("load", function () {
       }
     }
     draw(context) {
+      context.save()
+      context.translate(this.x, this.y)
+      context.rotate(this.angle)
       context.drawImage(
         this.image,
         this.frameX * this.spriteSize,
         this.frameY * this.spriteSize,
         this.spriteSize,
         this.spriteSize,
-        this.x,
-        this.y,
+        this.size * -0.5,
+        this.size * -0.5,
         this.size,
         this.size
       )
+      context.restore()
     }
   }
   class Player {
